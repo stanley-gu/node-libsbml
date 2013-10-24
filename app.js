3665c94936ec1bf03501bce79712902cb4d5eddc
Stanley Gu <stanleygu@gmail.com>
2013-10-24 01:32:23 -0700

    Simpler transmission of stdout and stderr

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var io = require('socket.io').listen(8004); // change to port for service to run
 io.sockets.on('connection', function(s) {
   var python = spawn('python', ['-i']);
 
-  python.stdout.pipe(process.stdout);
-  python.stderr.on('data', function (data) {
-    console.log('python stderr: ' + data);
-  });
+  //python.stdout.pipe(process.stdout);
+  //python.stderr.on('data', function (data) {
+  //  console.log('python stderr: ' + data);
+  //});
 
   python.on('close', function (code) {
     if (code !== 0) {
@@ -21,16 +21,24 @@ io.sockets.on('connection', function(s) {
     python.stdin.end();
   });
   s.on('run', function(input) {
-    console.log('Running libsbml.' + input.method);
-    python.stdin.write('libsbml.' + input.method + '(' + input.params.join(', ') + ')' + '\n');
+    //console.log('Running libsbml.' + input.method + '(' + input.params.join(', ') + ')' + '\n');
+    //python.stdin.write('libsbml.' + input.method + '(' + input.params.join(', ') + ')' + '\n');
+    console.log('Running command: ' + input.command);
+    python.stdin.write('libsbml.' + input.command + '\n');
     python.stdout.on('data', function(data) {
       // convert buffer to string
       var output = '' + data;
       s.emit('response', {
-        method: input.method,
         output: output
       });
-      console.log('python output: ' + output);
+      console.log('python stdout: ' + output);
+    });
+    python.stderr.on('data', function(data) {
+      var output = '' + data;
+      s.emit('response', {
+        error: output
+      });
+      console.log('python stderr: ' + output);
     });
   });
 });
